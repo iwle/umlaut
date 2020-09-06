93c500e15a546a9becb6f284aa5831ce30bb5219
Ian Wu <iwle@u.nus.edu>
2020-09-06 22:54:40 +0800

    Add unit tests

---
+++ test/test.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace test
+{
+	TEST_CLASS(test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::IsTrue(true);
+		}
+	};
+}
